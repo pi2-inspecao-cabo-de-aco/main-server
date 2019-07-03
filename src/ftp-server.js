9382e10ec9a98fd5f3ca6f67a50225a468f35734
Vitor Borges <vitorbertulucci@gmail.com>
2019-07-03 18:24:18 -0300

    Improve script to load zip files

--- src/ftp-server.js
+++ src/ftp-server.js
@@ -39,41 +39,52 @@ const execpromise = util.promisify(exec)
 
 async function main () {
   const TMP_PATH = '/tmp/comp'
-  const RICAS_IP = 'pi@192.168.4.12'
+  const RICAS_IP = 'pi@192.168.4.1'
   const RICAS_PASS = 'ricas123'
-  const LOCAL_PATH = '~/Projects/pi2/main-server/public/'
+  const LOCAL_PATH = '/home/vitor/Projects/unb/pi2/main-server/public/'
 
   console.log('----------> Executando comando de identificar arquivos gerados')
-  let { stdout, stderr } = await execpromise(`sshpass -p ${RICAS_PASS} ssh ${RICAS_IP} ls ${TMP_PATH}`)
-  if (stderr) {
-    console.log(stderr)
-  }
-
-  let backup = {}
-  let values = stdout.split('\n') // get filenames as array
-  for (let i of values) {
-    backup[i] = i // build key:value object to O(1) access, minimizing code complexity
-  }
-
-  // interval to check new files
-  setInterval(async () => {
-    // get new list
+  try {
     let { stdout, stderr } = await execpromise(`sshpass -p ${RICAS_PASS} ssh ${RICAS_IP} ls ${TMP_PATH}`)
     if (stderr) {
       console.log(stderr)
     }
-    let array = stdout.split('\n') // get filenames as array
-    let find = array.find(c => !backup[c]) // find new value
-    if (find) {
-      console.log(`--------------> Novo arquivo identificado: ${find}. Copiando arquivo`)
-      console.log(`--------------> Copiando `)
-      backup[find] = find // set new value as backup
-      let filepath = LOCAL_PATH + find
-      await execpromise(`sshpass -p ${RICAS_PASS} scp ${RICAS_IP}:${TMP_PATH}/${find} ${filepath}`) // copy file to local file system
-      console.log(`--------------> Arquivo copiado.`)
-      // await infoControll(filename)
+
+    let backup = {}
+    let values = stdout.split('\n') // get filenames as array
+    for (let i of values) {
+      if (i.length > 0) {
+        backup[i] = i // build key:value object to O(1) access, minimizing code complexity
+        console.log(`>>>>>>>>>>> Baixando ${i}`)
+        let filepath = LOCAL_PATH
+        await execpromise(`sshpass -p ${RICAS_PASS} scp ${RICAS_IP}:${TMP_PATH}/${i} ${filepath}`)
+        await infoControll(i)
+        console.log('>>>>>>>>>>>>>>> Done.')
+      }
     }
-  }, 5000)
+
+    // interval to check new files
+    setInterval(async () => {
+      // get new list
+      let { stdout, stderr } = await execpromise(`sshpass -p ${RICAS_PASS} ssh ${RICAS_IP} ls ${TMP_PATH}`)
+      if (stderr) {
+        console.log(stderr)
+      }
+      let array = stdout.split('\n') // get filenames as array
+      let newFilename = array.find(c => !backup[c]) // file new value
+      if (newFilename) {
+        console.log(`--------------> Novo arquivo identificado: ${newFilename}. Copiando arquivo`)
+        console.log(`--------------> Copiando....`)
+        backup[newFilename] = newFilename // set new value as backup
+        let filepath = LOCAL_PATH + newFilename
+        await execpromise(`sshpass -p ${RICAS_PASS} scp ${RICAS_IP}:${TMP_PATH}/${newFilename} ${filepath}`) // copy file to local file system
+        console.log(`--------------> Arquivo copiado.`)
+        await infoControll(newFilename)
+      }
+    }, 5000)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 main().then().catch()
